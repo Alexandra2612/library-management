75fc14c2abad7460667a2d04f69ba8c247480dff
Alexandra Hertanu <alexandra.hertanu@yahoo.com>
2020-05-06 22:24:32 +0300

    ADGUB-2 Added onClick action for registration button and created new registration scene

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,26 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class Controller {
+    @FXML
+    private void handleRegistrationButton(ActionEvent event) throws IOException {
+       Parent p=FXMLLoader.load(getClass().getResource("/fxml/registration.fxml"));
+       Scene scene2=new Scene(p,500,500);
+       Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+       window.setTitle("Register");
+       window.setScene(scene2);
+       window.show();
+    }
+
 }
