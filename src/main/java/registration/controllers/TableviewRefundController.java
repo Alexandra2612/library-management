fc7dfc5565ff40bddfab2563735059c11b9f5389
Bianca Ianc <bianca.ianc21@gmail.com>
2020-05-30 18:14:53 +0300

    ADGUB-27 Verificare depasire termen si implementare metoda goBack

--- src/main/java/registration/controllers/TableviewRefundController.java
+++ src/main/java/registration/controllers/TableviewRefundController.java
@@ -1,9 +1,12 @@
 package registration.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.beans.property.SimpleStringProperty;
@@ -12,11 +15,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import registration.exceptions.BookNotOwned;
 import registration.model.Imprumut;
@@ -36,7 +44,13 @@ public class TableviewRefundController implements Initializable {
     private Label refundMessageField;
 
     @FXML
-    private void goBack(ActionEvent actionEvent) {
+    private void goBack(ActionEvent actionEvent) throws IOException {
+        Parent p= FXMLLoader.load(getClass().getResource("/fxml/reader.fxml"));
+        Scene scene1=new Scene(p);
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setTitle("Pagina cititor");
+        window.setScene(scene1);
+        window.show();
     }
     public void initialize(URL location, ResourceBundle resources){
         dateColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Imprumut,String>, ObservableValue<String>>() {
@@ -78,7 +92,7 @@ public class TableviewRefundController implements Initializable {
         for(Imprumut i:im)
             if((i.getBook().getTitle()).equals(refundField.getText())){
                 ok=1;
-                exceedingtimecheck();
+                exceedingtimecheck(i);
             }
        if(ok==0) try {
             throw new BookNotOwned();
@@ -90,7 +104,16 @@ public class TableviewRefundController implements Initializable {
 
     }
 
-    private void exceedingtimecheck() {
-        refundMessageField.setText("ok");
+    private void exceedingtimecheck(Imprumut i) {
+        Date currentdate=new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(i.getDate());
+        c.add(Calendar.DATE, i.getBook().getDuration()); //same with c.add(Calendar.DAY_OF_MONTH, 1);
+        // convert calendar to date
+        Date currentDatePlus = c.getTime();
+        if(currentdate.after(currentDatePlus))
+            refundMessageField.setText("Limita de timp depasita");
+        else
+            refundMessageField.setText("se poate restitui");
     }
 }
