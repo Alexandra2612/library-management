abf3b00b1c1238efaeed5e0b2a9d0f7dde6fad04
Bianca Ianc <bianca.ianc21@gmail.com>
2020-06-02 22:36:38 +0300

    ADGUB-27 Calcul si afisare penalizari

--- src/main/java/registration/controllers/TableviewRefundController.java
+++ src/main/java/registration/controllers/TableviewRefundController.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -25,13 +25,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import registration.exceptions.BookNotOwned;
 import registration.model.Imprumut;
 import registration.model.ReaderUser;
+import registration.services.BookService;
 import registration.services.UserService;
-
-import static registration.controllers.TableViewReadersController.getSelectedUser;
 
 public class TableviewRefundController implements Initializable {
     @FXML
@@ -42,6 +40,8 @@ public class TableviewRefundController implements Initializable {
     @FXML private TableColumn<Imprumut,String> bookColumn;
     @FXML
     private Label refundMessageField;
+    @FXML
+    private Label refundMessageField1;
 
     @FXML
     private void goBack(ActionEvent actionEvent) throws IOException {
@@ -53,47 +53,52 @@ public class TableviewRefundController implements Initializable {
         window.show();
     }
     public void initialize(URL location, ResourceBundle resources){
-        dateColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Imprumut,String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Imprumut,String> c) {
-                DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-                return new SimpleStringProperty(df.format(c.getValue().getDate()));
-            }
+        dateColumn.setCellValueFactory(c -> {
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            Date date=c.getValue().getDate();
+            Calendar ca = Calendar.getInstance();
+            ca.setTime(date);
+
+            // manipulate date
+
+            ca.add(Calendar.DATE, c.getValue().getBook().getDuration()); //same with c.add(Calendar.DAY_OF_MONTH, 1);
+            // convert calendar to date
+            Date currentDatePlus = ca.getTime();
+            return new SimpleStringProperty(df.format(currentDatePlus));
         });
-        bookColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Imprumut,String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TableColumn.CellDataFeatures<Imprumut,String> c) {
-                return new SimpleStringProperty(c.getValue().getBook().getTitle()+" de "+c.getValue().getBook().getAuthor());
-            }
-        });
+        bookColumn.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getBook().getTitle()+" de "+c.getValue().getBook().getAuthor()));
         tableView.setItems(this.getImprumut());
     }
     public ObservableList<Imprumut> getImprumut(){
         ReaderUser r;
         r=UserService.getsomeUser(UserService.getConectedUser());
+        assert r != null;
         ArrayList<Imprumut> im= r.getListaimprumuturi();
         ObservableList<Imprumut> imp= FXCollections.observableArrayList();
         if(im==null)
-            im=new ArrayList<Imprumut>();
-        for(Imprumut i:im)
-            imp.add((Imprumut) i);
+            im= new ArrayList<>();
+        imp.addAll(im);
         return imp;
     }
 
 
 
     @FXML
-    private void handleRefundaction(ActionEvent actionEvent) {
+    private void handleRefundaction() {
         
         int ok=0;
         ReaderUser r;
         r=UserService.getsomeUser(UserService.getConectedUser());
+        assert r != null;
         ArrayList<Imprumut> im= r.getListaimprumuturi();
-        for(Imprumut i:im)
-            if((i.getBook().getTitle()).equals(refundField.getText())){
-                ok=1;
-                exceedingtimecheck(i);
-            }
+        try {
+            for (Imprumut i : im)
+                if ((i.getBook().getTitle()).equals(refundField.getText())) {
+                    ok = 1;
+                    exceedingtimecheck(i);
+
+                }
+        }catch(Exception ignored){}
        if(ok==0) try {
             throw new BookNotOwned();
         } catch (BookNotOwned bookNotOwned) {
@@ -110,10 +115,13 @@ public class TableviewRefundController implements Initializable {
         c.setTime(i.getDate());
         c.add(Calendar.DATE, i.getBook().getDuration()); //same with c.add(Calendar.DAY_OF_MONTH, 1);
         // convert calendar to date
+
         Date currentDatePlus = c.getTime();
-        if(currentdate.after(currentDatePlus))
-            refundMessageField.setText("Limita de timp depasita");
-        else
-            refundMessageField.setText("se poate restitui");
+
+        if(currentdate.after(currentDatePlus)) {
+            long diff = currentdate.getTime() - currentDatePlus.getTime();
+            refundMessageField1.setText("Limita de timp depasita, cost:"+TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)+" lei");
+        }
+
     }
 }
